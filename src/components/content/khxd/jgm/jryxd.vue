deff5497bf801afc2e138b547a8164e0c900e678
zqi <953762087@qq.com>
2019-03-25 22:44:23 +0800

    add: 今日已下单-增加表格action栏

--- src/components/content/khxd/jgm/jryxd.vue
+++ src/components/content/khxd/jgm/jryxd.vue
@@ -1,6 +1,6 @@
 <template lang='pug'>
   div.khxd-jgm-jryxd
-    Table(border ref="selection" :columns="jgmXdxxColumns" :data="data")
+    Table(border ref="selection" :columns="addColumn" :data="data")
 </template>
 
 <script>
@@ -11,18 +11,95 @@ export default {
   },
   data () {
     return {
-      data: [],
-      jgmXdxxColumns: JGM_XDXX_COLUMNS
+      data: [
+        {
+          bh: '20132',
+          zt: '普通',
+          khxm: '张三',
+          xdlx: '新订单',
+          dz: '四川省',
+          dh: '3133',
+          gq: '7',
+          bz: '发达',
+          cjr: '李四',
+          cjsj: '2019'
+        }
+      ],
+      jgmXdxxColumns: JGM_XDXX_COLUMNS,
+      actionColumn: {
+        title: '操作',
+        align: 'center',
+        width: 120,
+        render: (h, params) => {
+          return h('div', [
+            h('Icon', {
+              props: {
+                type: 'md-information-circle',
+                size: '15',
+                color: '#19be6b'
+              },
+              style: {
+                padding: '6px 4px',
+                cursor: 'pointer'
+              },
+              on: {
+                click: () => {
+                  this.change(params.index);
+                }
+              }
+            }),
+            h('Icon', {
+              props: {
+                type: 'md-create',
+                size: '15',
+                color: '#2db7f5'
+              },
+              style: {
+                padding: '6px 4px',
+                cursor: 'pointer'
+              },
+              on: {
+                click: () => {
+                  this.change(params.index);
+                }
+              }
+            }),
+            h('Icon', {
+              props: {
+                type: 'md-return-left',
+                size: '15',
+                color: '#ed4014'
+              },
+              style: {
+                padding: '6px 4px',
+                cursor: 'pointer'
+              },
+              on: {
+                click: () => {
+                  this.remove(params.index);
+                }
+              }
+            })
+          ]);
+        }
+      }
     };
   },
   computed: {
-
+    addColumn () {
+      this.jgmXdxxColumns.push(
+        this.actionColumn
+      );
+      return this.jgmXdxxColumns;
+    }
   },
   mounted() {
 
   },
   methods: {
-
+    change(idx) {
+      console.log(idx);
+    }
   }
 };
 </script>
